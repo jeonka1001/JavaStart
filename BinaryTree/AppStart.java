44ca36bd1cf565ba8f7dc3975d68303c999f9d71
jeonka1001 <52381846+jeonka1001@users.noreply.github.com>
2020-03-24 17:50:12 +0900

    Add files via upload

---
+++ BinaryTree/AppStart.java
@@ -0,0 +1,13 @@
+
+public class AppStart {
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.add(100);
+		bt.add(30);
+		bt.add(40);
+		bt.add(120);
+		bt.add(45);
+		
+		bt.searchStart();
+	}
+}
